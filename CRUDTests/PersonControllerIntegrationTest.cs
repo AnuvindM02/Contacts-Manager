92a0c0b050f7bba3df8ba072125e779f066badda
AnuvindM02 <anuvindm02@gmail.com>
2024-01-13 16:21:49 +0530

    Updated integration test to receive html dom object using fizzler

---
+++ CRUDTests/PersonControllerIntegrationTest.cs
@@ -0,0 +1,38 @@
+﻿using Fizzler.Systems.HtmlAgilityPack;
+using FluentAssertions;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDTests
+{
+    public class PersonControllerIntegrationTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+
+        public PersonControllerIntegrationTest(CustomWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+        [Fact]
+        #region Index
+        public async Task Index_ToReturnView()
+        {
+            HttpResponseMessage response = await _client.GetAsync("/Person/Index");
+
+            response.Should().BeSuccessful();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            HtmlDocument html = new HtmlDocument();
+            html.LoadHtml(responseBody);
+            var document = html.DocumentNode;
+
+            document.QuerySelectorAll("table.persons").Should().NotBeNull();
+        }
+        #endregion
+    }
+}
--- CRUDXunitTest/Program.cs
+++ CRUDXunitTest/Program.cs
@@ -24,10 +24,13 @@ if (builder.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 //Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PersonsDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
-Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
+if (builder.Environment.IsEnvironment("Test") == false)
+    Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
 
 app.UseStaticFiles();
 app.UseRouting();
 app.MapControllers(); 
 
 app.Run();
+
+public partial class Program { }
